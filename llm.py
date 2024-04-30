05e835f6f1c7a84af2fdacbaecb1fa2b968d22e2
rehmat11872 <raorehmat11@gmail.com>
2024-05-01 00:33:33 +0500

    lecture2

--- llm.py
+++ llm.py
@@ -1,8 +1,24 @@
-#from decouple import config
-
-#OPENAI_KEY = config('Define your key name')
-
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from langchain_openai import ChatOpenAI
+
+
+llm = ChatOpenAI(
+    model='gpt-3.5-turbo',
+    temperature=0.7
+)
+
+# response = llm.invoke("How are you")
+
+# response = llm.batch(['how are you', 'What is AI?'])
+
+response = llm.stream("How are you")
+
+
+
+for chunk in response:
+    print(chunk.content, end="", flush=True)
+
 
